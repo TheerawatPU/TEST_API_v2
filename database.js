f28323e836e82049bc2d5f906f5283c4b9857e2b
TheerawatPU <theerawat0258@gmail.com>
2024-10-20 19:09:54 +0700

    Initial commit

--- database.js
+++ database.js
@@ -1,28 +1,20 @@
 require('dotenv').config();
 
+
 const { Pool } = require('pg');
 
 // const pool = new Pool({
-//   user: 'default',
-//   password: 'pSTjM5hBqn1w',
-//   host: 'ep-divine-heart-a1lqglrp-pooler.ap-southeast-1.aws.neon.tech',
-//   port: 5432,
-//   database: 'verceldb',
-//   ssl: {
-//     rejectUnauthorized: false, // This allows connections without verifying the server's SSL certificate
-//   },
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   database: process.env.DB_NAME,
+//   ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false, // Handle SSL
 // });
 
-// comment v 2
-
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false, // Handle SSL
-});
+  connectionString: process.env.POSTGRES_URL,
+})
 
 pool.connect((err, client, release) => {
   if (err) {
